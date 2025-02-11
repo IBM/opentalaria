abc738c20588d05b43d6cf862a7db2371e2fd7d7
Vladimir Atanasov <vlado.atanasov@gmail.com>
2025-02-11 12:44:38 +0000

    added examples folder
    Signed-off-by: Vladimir Atanasov <vlado.atanasov@gmail.com>

--- go.mod
+++ go.mod
@@ -5,11 +5,11 @@ go 1.21.6
 require (
 	github.com/google/uuid v1.6.0
 	github.com/spf13/viper v1.19.0
-	github.com/traefik/yaegi v0.16.1
 	golang.org/x/sync v0.9.0
 )
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/ibm/opentalaria/config"
 	"github.com/ibm/opentalaria/logger"
-	"github.com/ibm/opentalaria/plugins"
 	// We start a web server only in localdev mode, which should't expose any sensitive information.
 	// If we add some web APIs one day, this functionality has to be reviewed.
 	// _ "expvar"
@@ -35,35 +36,24 @@ func initLogger(config *config.Config) {
 }
 
 func main() {
-	ps, err := plugins.New()
+	confFile := flag.String("c", "server.properties", "Path to config file. Default is server.properties")
+	flag.Parse()
+
+	// global config object that will be passed to all downstream APIs and methods
+	conf, err := config.NewConfig(*confFile)
 	if err != nil {
-		log.Printf("error init plugin: %v", err)
-	}
-	err = ps.Call()
-	if err != nil {
-		log.Printf("error invoking Call(): %+v\n", err)
+		slog.Error("Error initializing broker", "err", err)
+		os.Exit(1)
 	}
 
-	return
+	initLogger(conf)
 
-	// confFile := flag.String("c", "server.properties", "Path to config file. Default is server.properties")
-	// flag.Parse()
+	if conf.OTProfile == config.Localdev {
+		slog.Info(fmt.Sprintf("starting in local dev mode, listening on port :%d", conf.DebugServerPort))
+		// start a web server if we are in local dev mode
+		go http.ListenAndServe(fmt.Sprintf(":%d", conf.DebugServerPort), nil)
+	}
 
-	// // global config object that will be passed to all downstream APIs and methods
-	// conf, err := config.NewConfig(*confFile)
-	// if err != nil {
-	// 	slog.Error("Error initializing broker", "err", err)
-	// 	os.Exit(1)
-	// }
-
-	// initLogger(conf)
-
-	// if conf.OTProfile == config.Localdev {
-	// 	slog.Info(fmt.Sprintf("starting in local dev mode, listening on port :%d", conf.DebugServerPort))
-	// 	// start a web server if we are in local dev mode
-	// 	go http.ListenAndServe(fmt.Sprintf(":%d", conf.DebugServerPort), nil)
-	// }
-
-	// server := NewServer(conf)
-	// server.Run()
+	server := NewServer(conf)
+	server.Run()
 }
