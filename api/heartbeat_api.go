def127c1897a1ce950cc4ba1eb29b590a2144959
PawelK2012 <Kaim.paul@gmail.com>
2025-03-14 12:06:00 +0000

    Initial commit for HeartbeatAPI
    Signed-off-by: PawelK2012 <Kaim.paul@gmail.com>

--- api/heartbeat_api.go
+++ api/heartbeat_api.go
@@ -1,9 +1,19 @@
 package api
 
+import "opentalaria/protocol"
+
 type HeartbeatAPI struct {
 	Request Request
 }
 
 func (h HeartbeatAPI) Name() string {
 	return "Heartbeat"
+}
+
+func (h HeartbeatAPI) GetRequest() Request {
+	return h.Request
+}
+
+func (h HeartbeatAPI) GetHeaderVersion(requestVersion int16) int16 {
+	return (&protocol.MetadataResponse{Version: requestVersion}).GetHeaderVersion()
 }
